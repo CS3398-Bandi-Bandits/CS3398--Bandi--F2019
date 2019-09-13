f7a231c2aebefa72e0034a7dd9bfbc99739d70d7
José Herrera <jh1958@txstate.edu>
2019-09-12 20:10:58 -0500

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,23 @@
 package threesolid;
 
 // Author: Jose Herrera
-// Open Closed principle:
-// class is open to additions and closed to modifications
-// hello
+
+// Open Closed Principle:
+// Class is open to additions and closed to modifications.
+// New methods may be added but the simplicity of the existing
+// methods prevent any potential need for tampering.
+
+// Single Responsibility Principle:
+// The Manager has a single responsibility: to manage work.  It accepts a worker or eater
+// and allows it to either work or eat.  Although two seperate tasks they are,
+// together, one responsibility: Manage.
+
+// Interface Segregation Principal:
+// This principal applies more to the files containing the interface definitions but this 
+// Manager class employs it well enough.  It takes two simple interfaces that could have been written
+// together but have instead had their functionality split, and employs them more appropriately depending 
+// on the task required.  
+
 
 public class Manager {
 	
