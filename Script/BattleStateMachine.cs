4aaa547c298d106aa1eaf6c29586c9910f834e22
dmr122 <54727042+dmr122@users.noreply.github.com>
2019-10-07 20:38:39 -0500

    Add files via upload
    Unity Project is too many files to upload at once, I zipped the project and uploaded scripts separately

---
+++ Script/BattleStateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStateMachine : MonoBehaviour {
+
+    public enum PerformAction
+    {
+        WAIT,
+        TAKEACTION,
+        PERFORMACTION
+    }
+
+    public PerformAction performStates;
+    public List<TurnHandler> PerformList = new List<TurnHandler>();    //list of turnhandler classes
+    public List<GameObject> HeroesInBattle = new List<GameObject>();   //List for expansion to multiple heroes/enemies
+    public List<GameObject> EnemysInBattle = new List<GameObject>();   // ^^
+
+    //maybe public GameObject HeroInGame = new GameObject;
+    //      pulbic GameObject EnemyInGame = new GameObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+        performStates = PerformAction.WAIT;
+        EnemysInBattle.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        HeroesInBattle.AddRange(GameObject.FindGameObjectsWithTag("Hero"));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		switch (performStates)
+        {
+            case (PerformAction.WAIT):
+                break;
+            case (PerformAction.TAKEACTION):
+                break;
+            case (PerformAction.PERFORMACTION):
+                break;
+        }
+	}
+
+    public void CollectActions(TurnHandler input)
+    {
+        PerformList.Add(input); 
+    }
+}
