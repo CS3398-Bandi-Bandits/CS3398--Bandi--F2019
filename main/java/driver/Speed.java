34bcc0276ffe4d052282695958456e84ee85d13d
Devon Tyson <devonltyson@txstate.edu>
2019-10-18 14:25:50 -0500

    Added speed skill

---
+++ main/java/driver/Speed.java
@@ -0,0 +1,26 @@
+package FW;
+
+/**
+ * 	@author Devon Tyson
+ */
+
+public class Speed extends Skill {
+
+    int level, xp, goalXp;
+
+    public Speed() {
+        this.level = super.level;
+        this.xp = super.xp;
+        this.goalXp = super.goalXp;
+    }
+
+    @Override
+    public void train(int addedXp) {
+        super.train(addedXp);
+    }
+
+    @Override
+    public String toString() {
+        return "Speed";
+    }
+}
