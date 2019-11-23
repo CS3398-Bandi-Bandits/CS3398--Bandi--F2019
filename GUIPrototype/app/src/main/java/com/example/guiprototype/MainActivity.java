1025a193d292555b600e91d090a788af347c2991
Kenneth <kennethomo830@gmail.com>
2019-11-23 17:26:48 -0600

    Updated game with app

--- GUIPrototype/app/src/main/java/com/example/guiprototype/MainActivity.java
+++ GUIPrototype/app/src/main/java/com/example/guiprototype/MainActivity.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import com.WOPackage.FitWarriors.UnityPlayerActivity;//added unity object
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                    @Override
                    public void onClick(View v) {
                        openBattleActivity();
+
                    }
                });
 
@@ -104,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openBattleActivity() {
-        Intent intent = new Intent(this, BattleActivity.class);
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
         startActivity(intent);
     }
     public void openSettingsActivity() {
